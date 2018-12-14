0a03d81c929739f295c6fcd1079d2eaed9c147fc
mahuafeng@shipindiy.com <mahuafeng@shipindiy.com>
2018-12-14 18:31:23 +0800

    添加服务代码

--- testmodule/src/main/java/com/nmssdmf/testmodule/application/App.java
+++ testmodule/src/main/java/com/nmssdmf/testmodule/application/App.java
@@ -1,26 +1,23 @@
 package com.nmssdmf.testmodule.application;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.widget.ImageView;
+
+import android.content.Intent;
 
 import com.nmssdmf.commonlib.application.BaseApplication;
-import com.tmall.wireless.tangram.TangramBuilder;
-import com.tmall.wireless.tangram.util.IInnerImageSetter;
+import com.nmssdmf.testmodule.service.TestService;
 
 /**
  * Created by ${nmssdmf} on 2018/12/5 0005.
  */
 
 public class App extends BaseApplication{
+
+    private TestService service;
+
     @Override
     public void onCreate() {
         super.onCreate();
-        TangramBuilder.init(this, new IInnerImageSetter() {
-            @Override
-            public <IMAGE extends ImageView> void doLoadImageUrl(@NonNull IMAGE view, @Nullable String url) {
-
-            }
-        }, ImageView.class);
+        Intent intent = new Intent(this, TestService.class);
+        startService(intent);
     }
 }
