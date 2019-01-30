269c06114f95c91813aa0ea0b64aee01a448b6df
mahuafeng@shipindiy.com <mahuafeng@shipindiy.com>
2019-01-30 15:26:42 +0800

    添加rtsp代码

--- testmodule/src/main/java/com/nmssdmf/testmodule/rtsp/RtspActivity.java
+++ testmodule/src/main/java/com/nmssdmf/testmodule/rtsp/RtspActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.VideoView;
 
 import com.nmssdmf.testmodule.R;
@@ -12,6 +13,7 @@ import com.nmssdmf.testmodule.R;
 public class RtspActivity extends AppCompatActivity {
     private VideoView vv;
     private Button btn;
+    private EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +21,16 @@ public class RtspActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rtsp);
 
 
-        final String url = "rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov";
+//        final String url = "rtsp://184.72.239.149/vod/mp4://BigBuckBunny_175k.mov";
 
         vv = findViewById(R.id.vv);
         btn = findViewById(R.id.btn);
+        et = findViewById(R.id.et);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                vv.setVideoURI(Uri.parse(url));
+                vv.setVideoURI(Uri.parse(et.getText().toString()));
                 vv.requestFocus();
                 vv.start();
             }
