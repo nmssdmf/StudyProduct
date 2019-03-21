2cdc389172bf36e493adc87e4dfaa27c8520108e
nmssdmf <mhf103026@163.com>
2019-03-22 00:03:20 +0800

    完善只是储备：activity启动方式，生命周期，设计模式，内存泄漏 添加单例模式

---
+++ testmodule/src/main/java/com/nmssdmf/testmodule/designmode/Singleton.java
@@ -0,0 +1,29 @@
+package com.nmssdmf.testmodule.designmode;
+
+/**
+ * 单例模式：单例模式有以下几种实现方式
+ *      1、懒汉模式
+ *      2、Double Check Lock
+ *      3、静态内部类单例模式
+ *      4、枚举单例模式
+ *      5、使用容器实现单例模式
+ *      最合理的实现方式是静态内部类单例模式:
+ *          当第一次加载Singleton类时并不会初始化sInstance，只有在第一次调用Singleton的getInstance方法时
+ *          才会导致sInstance被初始化，因此，第一次调用getInstance方法会导致虚拟机加载SingletonHolder类，
+ *          这种方式不仅能够确保线程安全，也能够保证单例对象的唯一性，同时也延迟了单例的实例化，所以这是
+ *          推荐使用的单例模式实现方式。
+ */
+public class Singleton {
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        return SingletonHolder.sInstance;
+    }
+
+    /**
+     * 静态内部类
+     */
+    private static class SingletonHolder {
+        private static final Singleton sInstance = new Singleton();
+    }
+}
